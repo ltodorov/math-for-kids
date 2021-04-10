6bac63c9877654de7d5b1309a3568d2a1811454b
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2021-04-10 17:40:17 +0300

    Improve setTextContent test

--- src/scripts/helpers/set-input-value.test.ts
+++ src/scripts/helpers/set-input-value.test.ts
@@ -1,13 +1,13 @@
 import { setInputValue } from "./set-input-value";
 
 describe("setInputValue", () => {
-    test("should not set value if not HTMLInputElement", () => {
+    it("should not set value if not HTMLInputElement", () => {
         const input = document.createElement("div");
         setInputValue(input, "1");
         expect(input.textContent).toBe("");
     });
 
-    test("should set value if HTMLInputElement", () => {
+    it("should set value if HTMLInputElement", () => {
         const input = document.createElement("input");
         setInputValue(input, "1");
         expect(input.value).toBe("1");
--- src/scripts/helpers/set-text-content.test.ts
+++ src/scripts/helpers/set-text-content.test.ts
@@ -1,11 +1,11 @@
 import { setTextContent } from "./set-text-content";
 
 describe("setTextContent", () => {
-    test("should not throw error if the element is null", () => {
+    it("should not throw error if the element is null", () => {
         expect(() => setTextContent(null, "")).not.toThrow();
     });
 
-    test("should set text content", () => {
+    it("should set text content", () => {
         const element = document.createElement("div");
         setTextContent(element, "test");
         expect(element.textContent).toBe("test");
