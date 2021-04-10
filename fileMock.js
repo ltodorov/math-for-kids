f2a0ba18aff1cde1d4f64d7ff187118b62733869
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2021-04-10 17:37:02 +0300

    fileMock readability

--- fileMock.js
+++ fileMock.js
@@ -4,6 +4,7 @@ module.exports = {
     process(src, filename, config, options) {
         return `module.exports = ${JSON.stringify(path.basename(filename))};`;
     },
+
     getCacheKey() {
         return "fileMock";
     }
