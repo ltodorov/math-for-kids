ba10b0ffde800bcb3b2e67518f3d7e7491daa246
Lyubomir Todorov <lyubomir.todorov@gmail.com>
2022-05-04 12:45:53 +0300

    Fix transformer

--- fileMock.js
+++ fileMock.js
@@ -2,7 +2,9 @@ const path = require("path");
 
 module.exports = {
     process(src, filename, config, options) {
-        return `module.exports = ${JSON.stringify(path.basename(filename))};`;
+        return {
+            code: `module.exports = ${JSON.stringify(path.basename(filename))};`,
+        };
     },
 
     getCacheKey() {
